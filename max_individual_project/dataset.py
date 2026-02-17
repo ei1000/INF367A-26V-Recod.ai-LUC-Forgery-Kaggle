bb4fbaa08cf38bdfa3d7f2f1a3456fb3e31e97a5
Max Bakke Seymour <max.bakke.seymour@gmail.com>
2026-02-17 17:06:45 +0100

    Get dataset class and update main runner file

---
+++ max_individual_project/dataset.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torchvision.transforms import v2, InterpolationMode
+from PIL import Image
+
+def make_transform(resize_size: int=384):
+    to_tensor = v2.ToImage()
+    resize = v2.Resize((resize_size, resize_size), antialias=True)
+    to_float = v2.ToDtype(torch.float32, scale=True)
+    normalize = v2.Normalize(
+        mean=(0.485, 0.456, 0.406),
+        std=(0.229, 0.224, 0.225)
+    )
+    return v2.Compose([to_tensor, resize, to_float, normalize])
+
+
+class ForgeryDataset(Dataset):
+    def __init__(self, samples, mask_dir: Path | None=None, size=384):
+        self.samples = samples
+        self.mask_dir = mask_dir
+
+        self.size = size
+
+        self.image_transforms = make_transform(self.size)
+
+        self.mask_transforms = v2.Compose([
+            v2.Resize((self.size, self.size), 
+            interpolation=InterpolationMode.NEAREST),
+        ])
+    
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        img_path, label = self.samples[idx]
+
+        image = Image.open(img_path).convert("RGB")
+        image = self.image_transforms(image)
+
+        if self.mask_dir is not None and "forged" in img_path.parent.name:
+            mask_path = self.mask_dir / img_path.name.replace(".png", ".npy")
+            mask = np.load(mask_path)
+            mask = torch.from_numpy(mask)
+            if mask.ndim == 2:
+                mask = mask.unsqueeze(0)
+            mask = self.mask_transforms(mask)
+            mask = mask.squeeze(0).long()
+        else:
+            mask = torch.from_numpy(np.zeros((256,  256)))
+
+        return image, mask, label
+
---
+++ max_individual_project/main.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import torch
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from dataset import ForgeryDataset
+
+
+from cnn_feature_extractor import PyramidFeatureExtractor
+from zernike_feature_extractor import PyramidZernikeExtractor
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load data
+root = Path('data')
+train_image_folder = ImageFolder(root / "train_images")
+
+samples = [(Path(p), y) for p, y in train_image_folder.samples]
+
+train_dataset = ForgeryDataset(
+    samples=samples,
+    mask_dir=root / "train_masks",
+)
+
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=False)
+
+# Feature extraction
+pyramid_bb = PyramidFeatureExtractor().to(device)
+
+pq_list = [(0,0),(1,1),(0,2),(1,3),(0,4),(1,5)]
+pyramid_zm = PyramidZernikeExtractor(pq_list, kernel_size=64).to(device)
+
+for images, _, _ in train_loader:
+    images = images.to(device)
+
+    Fcb, Fco, Fcu = pyramid_bb(images)
+    Fzb, Fzo, Fzu = pyramid_zm(images)
+    break
+
+print(Fcb)
+print(Fco)
+print(Fcu)
+
+
+print(Fzb)
+print(Fzo)
+print(Fzu)
